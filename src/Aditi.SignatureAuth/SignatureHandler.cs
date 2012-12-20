27c5bf95f0993179b72a855fcdffa675ccbddc2f
dunnry <dunnry@aditi.com>
2012-12-20 12:50:17 -0500

    Handling more error conditions with missing or malformed Authorization headers.

--- src/Aditi.SignatureAuth/SignatureHandler.cs
+++ src/Aditi.SignatureAuth/SignatureHandler.cs
@@ -21,6 +21,11 @@ namespace Aditi.SignatureAuth
             Signature signature = null;
             try
             {
+                if (request.Headers.Authorization == null)
+                {
+                    return UnAuthorized(request, "Missing Authorization Header");
+                }
+
                 // Why do I have to replace "&quot;" with an actual quote? The actual header is correct,
                 // but something is doing some sort of XML/HTML entity escaping. Do we need to undo
                 // other kinds of escaping?
@@ -28,37 +33,33 @@ namespace Aditi.SignatureAuth
             }
             catch (ArgumentException e)
             {
-                return Task.Factory.StartNew(() =>
-                    {
-                        var response = request.CreateResponse(HttpStatusCode.Unauthorized);
-                        response.Content = new StringContent(e.Message);
-                        return response;
-                    });
+                return UnAuthorized(request, e.Message);
             }
+
             signature.SecretKey = Convert.FromBase64String(KeyLookup(signature.Id));
             if (signature.Mac != signature.ComputeMac())
             {
-                return Task.Factory.StartNew(() =>
-                {
-                    var response = request.CreateResponse(HttpStatusCode.Unauthorized);
-                    response.Content = new StringContent("Invalid MAC. Signatures don't match.");
-                    return response;
-                });
+                return UnAuthorized(request, "Invalid MAC. Signatures don't match.");
             }
             else if ((DateTimeOffset.UtcNow - signature.Timestamp).Duration() > TimeSpan.FromMinutes(5))
             {
-                return Task.Factory.StartNew(() =>
-                    {
-                        var response = request.CreateResponse(HttpStatusCode.Unauthorized);
-                        response.Content = new StringContent("Invalid MAC. Timestamp must be within five minutes of the current time.");
-                        return response;
-                    });
+                return UnAuthorized(request, "Invalid MAC. Timestamp must be within five minutes of the current time.");
             }
             else
             {
                 request.Properties["tenantId"] = signature.Id;
                 return base.SendAsync(request, cancellationToken);
             }
+        }
+  
+        private Task<HttpResponseMessage> UnAuthorized(HttpRequestMessage request, string message)
+        {
+            return Task.Factory.StartNew(() =>
+            {
+                var response = request.CreateResponse(HttpStatusCode.Unauthorized);
+                response.Content = new StringContent(message);
+                return response;
+            });
         }
     }
 }
